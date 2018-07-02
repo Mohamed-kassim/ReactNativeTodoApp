d570df4df0e029cd49fbd7440efd04321c43a675
MohamedKassim <--global>
2018-07-02 18:25:19 +0200

    Todo header

--- index.js
+++ index.js
@@ -1,4 +1,23 @@
-import { AppRegistry } from 'react-native';
-import App from './App';
+// import { AppRegistry } from 'react-native';
+// import App from './App';
+//
+// AppRegistry.registerComponent('ReactNativeTodoApp', () => App);
+// import needed libraries
+import React from 'react';
+import {AppRegistry, Text, View} from 'react-native';
+import Header from './app/Header'
 
-AppRegistry.registerComponent('ReactNativeTodoApp', () => App);
+// create the component
+const sample = () => {
+    return(
+        <View >
+        <Header/>
+        <Text> Hello world </Text>
+        </View>
+    );
+};
+// styles
+
+// render the component on the device or import it
+// u must have registered component
+AppRegistry.registerComponent('ReactNativeTodoApp', ()=> sample );
